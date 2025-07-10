21ec29931df8ced4545af738ecf190d0649ceab2
ghigolini <jacsalv@outlook.it>
2025-07-10 13:21:48 +0200

    a

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { useEffect, useRef, useState } from "react";
 import ChatBox from "./components/chatBox";
 import NavBar from "./components/NavBar";
+import { ThemeProvider } from "@material-tailwind/react";
 
 function App() {
 	const [message, set_message] = useState("");
@@ -16,13 +17,13 @@ function App() {
 			data: message_data,
 			headers: { "Content-Type": "multipart/form-data" },
 		})
-		.then((response) => {
-			console.log(response.data.answer);
-			set_chat([...latestChatUpdate.current, "**Bot**\n" + response.data.answer]);
-		})
-		.catch((error) => {
-			console.error("Error: " + error);
-		});
+			.then((response) => {
+				console.log(response.data.answer);
+				set_chat([...latestChatUpdate.current, "**Bot**\n" + response.data.answer]);
+			})
+			.catch((error) => {
+				console.error("Error: " + error);
+			});
 	}
 
 	const send_message = async (e) => {
@@ -41,7 +42,7 @@ function App() {
 
 			await call_api(message_data);
 
-			set_message("");
+			await set_message("");
 		}
 	};
 
@@ -60,31 +61,33 @@ function App() {
 	}, [chat]);
 
 	return (
-		<div className="bg-neutral-800 min-h-screen grid grid-cols-10 text-lg">
-			<div className="hidden lg:block col-span-1 bg-red-200 lg:fixed h-full w-[200px]">
-				<NavBar set_chat={set_chat} />
-			</div>
-
-			<div className="lg:ml-[200px] col-span-10 lg:col-span-10 flex flex-col justify-between items-center px-10 py-8 bg-neutral-900">
-				<div ref={container_ref} className="flex-1 w-full overflow-y-auto mb-4 pb-10">
-					{chat.map((msg, index) => (
-						<ChatBox key={index} value={msg} />
-					))}
+		<ThemeProvider>
+			<div className="bg-gray-800 min-h-screen grid grid-cols-10 text-lg">
+				<div className="hidden lg:block col-span-1 bg-red-200 lg:fixed h-full w-[200px]">
+					<NavBar set_chat={set_chat} />
 				</div>
 
-				<form onSubmit={send_message} className="w-full fixed bottom-10 flex justify-center">
-					<input
-						name="message-box"
-						className="bg-neutral-800 text-white py-2 px-4 rounded-l-lg border border-neutral-600 w-2/3"
-						type="text"
-						placeholder="Write something..."
-						value={message}
-						onChange={(e) => set_message(e.target.value)}
-					/>
-					<input className="bg-green-500 text-white font-bold py-2 px-6 rounded-r-lg hover:bg-green-700" type="submit" value="Send" />
-				</form>
+				<div className="lg:ml-[200px] col-span-10 lg:col-span-10 flex flex-col justify-between items-center px-10 py-8 bg-gray-900">
+					<div ref={container_ref} className="flex-1 w-full overflow-y-auto mb-4 pb-10">
+						{chat.map((msg, index) => (
+							<ChatBox key={index} value={msg} />
+						))}
+					</div>
+
+					<form onSubmit={send_message} className="w-full fixed bottom-10 flex justify-center">
+						<input
+							name="message-box"
+							className="bg-gray-800 text-white py-2 px-4 rounded-l-lg border border-gray-600 w-2/3"
+							type="text"
+							placeholder="Write something..."
+							value={message}
+							onChange={(e) => set_message(e.target.value)}
+						/>
+						<input className="bg-green-500 text-white font-bold py-2 px-6 rounded-r-lg hover:bg-green-700" type="submit" value="Send" />
+					</form>
+				</div>
 			</div>
-		</div>
+		</ThemeProvider>
 	);
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,158 @@
+import { Button, Switch, Typography } from "@material-tailwind/react";
 import axios from "axios";
-const logo = require("../assets/logo_cb.png")
+import { useEffect, useRef, useState } from "react";
+const logo = require("../assets/logo_cb.png");
 
-function NavBar({set_chat}) {
-    const reset_chat = async (e) => {
-        await axios({
-            method: 'get',
-            url: 'http://127.0.0.1:5000/api/chat/reset',
-            headers: {'Content-Type': 'multipart/form-data'}
-        })
-        .then((response) => {
-            console.log(response.data.answer);
-            set_chat([]);
-        })
-        .catch((error) => {
-            console.error("Error: " + error);
-        });
+function NavBar({ set_chat }) {
+	const fileInputRef = useRef(null);
+	const [fileNames, setFileNames] = useState([]);
 
+	const reset_chat = async (e) => {
+		await axios({
+			method: "get",
+			url: "http://127.0.0.1:5000/api/chat/reset",
+			headers: { "Content-Type": "multipart/form-data" },
+		})
+			.then((response) => {
+				console.log(response.data);
+				set_chat([]);
+			})
+			.catch((error) => {
+				console.error("Error: " + error);
+			});
+	};
+
+	const handleClick = () => {
+		fileInputRef.current?.click();
+	};
+
+	const handleFileChange = (event) => {
+    const files = event.target.files;
+    if (files) {
+      const names = Array.from(files).map(file => file.name);
+      setFileNames(names);
+    } else {
+      setFileNames([]);
     }
-    return (
-        <div className="bg-neutral-800 border-r-2 p-10 border-r-neutral-700 text-center h-full bg-neutral-800">
-            <div>
-                <img src={logo}></img>
-            </div>
-            <button className="bg-red-500 text-white font-bold py-2 px-10 rounded hover:bg-red-700" onClick={reset_chat}>Reset</button>
-        </div>
-    );
+  };
+
+
+	const [guardrails, setGuardrails] = useState(false);
+	const [rag, setRag] = useState(false);
+
+	useEffect(() => {
+		axios({
+			method: "post",
+			url: "http://127.0.0.1:5000/api/chat/guardrails",
+			data: { value: guardrails },
+			headers: { "Content-Type": "multipart/form-data" },
+		})
+			.then((response) => {
+				console.log(response.data);
+			})
+			.catch((error) => {
+				console.error("Error: " + error);
+			});
+	}, [guardrails]);
+
+	useEffect(() => {
+		axios({
+			method: "post",
+			url: "http://127.0.0.1:5000/api/chat/rag",
+			data: { value: rag },
+			headers: { "Content-Type": "multipart/form-data" },
+		})
+			.then((response) => {
+				console.log(response.data);
+			})
+			.catch((error) => {
+				console.error("Error: " + error);
+			});
+	}, [rag]);
+
+	const ragApiCall = (e) => {
+		e.preventDefault();
+
+		axios({
+			method: "post",
+			url: "http://127.0.0.1:5000/api/chat/rag",
+			data: { value: rag },
+			headers: { "Content-Type": "multipart/form-data" },
+		})
+			.then((response) => {
+				console.log(response.data.answer);
+			})
+			.catch((error) => {
+				console.error("Error: " + error);
+			});
+	};
+
+	return (
+		<div className="bg-gray-800 border-r-2 p-10 border-r-gray-700 h-full bg-gray-800">
+			<div>
+				<img src={logo} alt="Logo"></img>
+			</div>
+			<div className="flex justify-center min-h-screen">
+				<ul className="flex flex-col gap-5">
+					<li>
+						<Button className="bg-red-500 text-white font-bold px-10 py-2 rounded hover:bg-red-700" onClick={reset_chat}>
+							Reset
+						</Button>
+					</li>
+					<li>
+						<Switch
+							color="green"
+							label={
+								<div>
+									<Typography className="text-gray-50 font-xl">Guardrails</Typography>
+								</div>
+							}
+							onClick={() => {
+								setGuardrails(!guardrails);
+							}}
+						/>
+					</li>
+					<li>
+						<Switch
+							color="green"
+							label={
+								<div>
+									<Typography className="text-gray-50 font-xl" onClick={ragApiCall}>
+										RAG
+									</Typography>
+								</div>
+							}
+							onClick={() => {
+								setRag(!rag);
+							}}
+						/>
+					</li>
+					<li>
+						<form>
+							<input multiple type="file" ref={fileInputRef} onChange={handleFileChange} className="hidden" />
+							<Button className={`mb-5 ${fileInputRef !== null ? "bg-green-600" : "bg-black"}`} disabled={rag ? false : true} onClick={handleClick}>
+								Select RAG files
+							</Button>
+							<div className="text-gray-50">
+								{fileInputRef !== null ? "File uploaded" : "No file uploaded"}
+							</div>
+							{fileNames.length > 0 && (
+								<ul className="space-y-1">
+									{fileNames.map((name, index) => (
+										<li key={index} className="break-words">
+											<div className="bg-gray-700 p-2 rounded text-sm text-gray-50">
+												{index+1}. {name}
+											</div>
+										</li>
+									))}
+								</ul>
+							)}
+						</form>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default NavBar;
